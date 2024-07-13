1c4da1f5104e984346d62832f518fa8e0c1f72b5
kowshikk007 <kowshikk786@gmail.com>
2024-07-13 19:11:02 +0530

    Delete plan life Detection directory

--- plan life Detection/digipathos/__main__.py
+++
@@ -1,9 +0,0 @@
-from digipathos.cli.browser import init_interactive_mode
-
-
-def run():
-    init_interactive_mode()
-
-
-if __name__ == '__main__':
-    run()
--- plan life Detection/digipathos/constants.py
+++
@@ -1,2 +0,0 @@
-BASE_URL = 'https://www.digipathos-rep.cnptia.embrapa.br'
-ENDPOINT_ITEMS = '/jspui/zipsincollection/123456789/3/?offset=0&limit=270'
